3cd99b6950f45290e8d8e2033547d1bf0da077d4
tári_veronika <tarivera1998@gmail.com>
2023-11-26 19:06:12 +0100

    Minor fixes in Zoo class

--- src/main/java/hu/unideb/inf/zoo_animals/model/Zoo.java
+++ src/main/java/hu/unideb/inf/zoo_animals/model/Zoo.java
@@ -14,14 +14,14 @@ public class Zoo {
     @Column(name="name", unique = true)
     private String zooName;
     @Column(name="location")
-    private String zooLocation;
+    private String location;
 
     public Zoo() {
     }
     public Zoo(Long id, String name, String location) {
         this.zooId = id;
         this.zooName = name;
-        this.zooLocation = location;
+        this.location = location;
     }
     public Long getId() {
         return zooId;
@@ -36,28 +36,28 @@ public class Zoo {
         this.zooName = name;
     }
     public String getLocation() {
-        return zooLocation;
+        return location;
     }
     public void setLocation(String location) {
-        this.zooLocation = location;
+        this.location = location;
     }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Zoo zoo = (Zoo) o;
-        return Objects.equals(zooId, zoo.zooId) && Objects.equals(zooName, zoo.zooName) && Objects.equals(zooLocation, zoo.zooLocation);
+        return Objects.equals(zooId, zoo.zooId) && Objects.equals(zooName, zoo.zooName) && Objects.equals(location, zoo.location);
     }
     @Override
     public int hashCode() {
-        return Objects.hash(zooId, zooName, zooLocation);
+        return Objects.hash(zooId, zooName, location);
     }
     @Override
     public String toString() {
         return "Zoo{" +
                 "id=" + zooId +
                 ", name='" + zooName + '\'' +
-                ", location='" + zooLocation + '\'' +
+                ", location='" + location + '\'' +
                 '}';
     }
 }
