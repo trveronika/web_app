bfbc89ed34bffb309fa67075a53446af24ee8725
tári_veronika <tarivera1998@gmail.com>
2023-11-26 16:39:37 +0100

    Implemented the basic services

---
+++ src/main/java/hu/unideb/inf/zoo_animals/service/impl/AnimalServiceImpl.java
@@ -0,0 +1,52 @@
+package hu.unideb.inf.zoo_animals.service.impl;
+
+import hu.unideb.inf.zoo_animals.model.Animal;
+import hu.unideb.inf.zoo_animals.repository.AnimalRepository;
+import hu.unideb.inf.zoo_animals.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnimalServiceImpl implements AnimalService {
+    private final AnimalRepository animalRepository;
+
+    @Autowired
+    public AnimalServiceImpl(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    @Override
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+    @Override
+    public Animal getAnimalById(Long id) {
+        return animalRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveAnimal(Animal animal) {
+        animalRepository.save(animal);
+    }
+
+    @Override
+    public void updateAnimal(Long id, Animal updatedAnimal) {
+        Animal existingAnimal = animalRepository.findById(id).orElse(null);
+        if (existingAnimal != null) {
+            existingAnimal.setType(updatedAnimal.getType());
+            existingAnimal.setName(updatedAnimal.getName());
+            existingAnimal.setAge(updatedAnimal.getAge());
+            existingAnimal.setColour(updatedAnimal.getColour());
+            existingAnimal.setZoo(updatedAnimal.getZoo());
+            animalRepository.save(existingAnimal);
+        }
+    }
+
+    @Override
+    public void deleteAnimal(Long id) {
+        animalRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/hu/unideb/inf/zoo_animals/service/impl/ZooServiceImpl.java
@@ -0,0 +1,49 @@
+package hu.unideb.inf.zoo_animals.service.impl;
+
+import hu.unideb.inf.zoo_animals.model.Zoo;
+import hu.unideb.inf.zoo_animals.repository.ZooRepository;
+import hu.unideb.inf.zoo_animals.service.ZooService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ZooServiceImpl implements ZooService {
+    private final ZooRepository zooRepository;
+
+    @Autowired
+    public ZooServiceImpl(ZooRepository zooRepository) {
+        this.zooRepository = zooRepository;
+    }
+
+    @Override
+    public List<Zoo> getAllZoos() {
+        return zooRepository.findAll();
+    }
+
+    @Override
+    public Zoo getZooById(Long id) {
+        return zooRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void saveZoo(Zoo zoo) {
+        zooRepository.save(zoo);
+    }
+
+    @Override
+    public void updateZoo(Long id, Zoo updatedZoo) {
+        Zoo existingZoo = zooRepository.findById(id).orElse(null);
+        if (existingZoo != null) {
+            existingZoo.setName(updatedZoo.getName());
+            existingZoo.setLocation(updatedZoo.getLocation());
+            zooRepository.save(existingZoo);
+        }
+    }
+
+    @Override
+    public void deleteZoo(Long id) {
+        zooRepository.deleteById(id);
+    }
+}
