04af6134043d043920605c985213f8b2887e5caa
tári_veronika <tarivera1998@gmail.com>
2023-11-27 16:34:32 +0100

    Added the controllers

--- src/main/java/hu/unideb/inf/zoo_animals/controller/AnimalController.java
+++ src/main/java/hu/unideb/inf/zoo_animals/controller/AnimalController.java
@@ -1,4 +1,43 @@
 package hu.unideb.inf.zoo_animals.controller;
 
+import hu.unideb.inf.zoo_animals.dto.AnimalDto;
+import hu.unideb.inf.zoo_animals.service.AnimalService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class AnimalController {
+    private final AnimalService animalService;
+
+    public AnimalController(AnimalService animalService) {
+        this.animalService = animalService;
+    }
+    @GetMapping
+    public ResponseEntity<List<AnimalDto>> getAllAnimals() {
+        List<AnimalDto> dogs = animalService.getAllAnimals();
+        return new ResponseEntity<>(dogs, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<AnimalDto> getAnimalById(@PathVariable Long id) {
+        AnimalDto animal = animalService.getAnimalById(id);
+        return animal != null ? ResponseEntity.ok(animal) : ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    public ResponseEntity<Void> saveAnimal(@RequestBody AnimalDto animalDto) {
+        animalService.saveAnimal(animalDto);
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateAnimal(@PathVariable Long id, @RequestBody AnimalDto updatedAnimalDto) {
+        animalService.updateAnimal(id, updatedAnimalDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAnimal(@PathVariable Long id) {
+        animalService.deleteAnimal(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/hu/unideb/inf/zoo_animals/controller/ZooController.java
+++ src/main/java/hu/unideb/inf/zoo_animals/controller/ZooController.java
@@ -1,16 +1,52 @@
 package hu.unideb.inf.zoo_animals.controller;
 
+import hu.unideb.inf.zoo_animals.dto.ZooDto;
 import hu.unideb.inf.zoo_animals.repository.ZooRepository;
+import hu.unideb.inf.zoo_animals.service.ZooService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/zoos")
 public class ZooController {
     @Autowired
-    ZooRepository zooRepository;
+    private final ZooService zooService;
 
+    public ZooController(ZooService zooService) {
+        this.zooService = zooService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ZooDto>> getAllZoos() {
+        List<ZooDto> zoos = zooService.getAllZoos();
+        return ResponseEntity.ok(zoos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ZooDto> getZooById(@PathVariable Long id) {
+        ZooDto zoo = zooService.getZooById(id);
+        return zoo != null ? ResponseEntity.ok(zoo) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveZoo(@RequestBody ZooDto zooDto) {
+        zooService.saveZoo(zooDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateZoo(@PathVariable Long id, @RequestBody ZooDto updatedZooDto) {
+        zooService.updateZoo(id, updatedZooDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteZoo(@PathVariable Long id) {
+        zooService.deleteZoo(id);
+        return ResponseEntity.ok().build();
+    }
 }
